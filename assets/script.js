59fd32bdc7280fe95ffbb2f39c2ff713a419fc1e
rudyxwhite <brandonrwing98@gmail.com>
2023-01-21 17:22:39 -0600

    starting over

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,53 @@
 //Question pool array for the quiz
 
-var questions = [
+const questions = [
     {
         q: "Which of the following data types refers to a value that is either True or False?",
-        a: ["Boolean", "Strings", "Arrays", "Numbers"],
+        a: "Boolean",
+        b: "Strings",
+        c: "Arrays",
+        d: "Numbers",
         correct: "Boolean"
     },
     {
         q: "What is the correct syntax of linking a JavaScript file to an HTML page?",
-        a: ["<java src> </java>", "<js src> </js>", "<script src> </script>", "<javascript src> </javascript>"],
+        a: "<java src> </java>",
+        b: "<js src> </js>",
+        c: "<script src> </script>",
+        d: "<javascript src> </javascript>",
         correct: "<script src> </script>"
     },
     {
         q: "Which javascript method is used to merge two or more arrays?",
-        a: ["merge", "combine", "return", "concat"],
+        a: "merge",
+        b: "combine",
+        c: "return",
+        d: "concat",
         correct: "concat"
     },
     {
         q: "The operations of a function are stored within: ",
-        a: ["( )", "[ ]", "{ }", "< >"],
+        a: "( )",
+        b: "[ ]",
+        c: "{ }",
+        d: "< >",
         correct: "{ }"
     },
     {
         q: "What does CSS stand for?",
-        a: ["Computer Styling System", "Cascading Style Sheets", "Creating Style Scripts", "None of the above"],
+        a: "Computer Styling System",
+        b: "Cascading Style Sheets",
+        c: "Creating Style Scripts",
+        d: "None of the above",
         correct: "Cascading Style Sheets"
-    },
-    {
-        q: "?",
-        a: ["css", "class", "src", "style"],
-        correct: ""
     }
 ]
 
 //Next, time for DOM elements
-
-const questionEl = document.querySelector("#question-text")
-
-const timerEl = document.querySelector("#timer")
-
-const optionsEl = document.querySelector("#option-text")
-
-
-const questionNumber=0;
-let timeleft = 60;
-
-function showQuestion() {
-    let currentQuestion = questions[questionNumber];
-  const promptEl = document.getElementById("question-text")
-    questionEl.textContent = currentQuestion.q;
-
-}
-
-showQuestion();
+const beginEl = document.getElementById("beginquiz")
+const questionEl = document.getElementById("question")
+const optionOneEl = document.getElementById("option-one")
+const optionTwoEl = document.getElementById("option-two")
+const optionThreeEl = document.getElementById("option-three")
+const optionFourEl = document.getElementById("option-four")
+const timerEl = document.getElementById("timer")
